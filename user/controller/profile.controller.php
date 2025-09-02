e88c4a486a9458671eada1b672a5905af5ef1190
unknown <ianjeru020@gmail.com>
2025-09-03 00:07:08 +0300

    Created a profile update controller module

---
+++ user/controller/profile.controller.php
@@ -0,0 +1,38 @@
+<?php
+
+include "./../autoload/autoloader.php";
+
+$profile = new profile();
+
+$id = $_SESSION['user_id'];
+
+$result = $profile->personalDetails($id);
+
+    $first_name = ucwords($result['first_name']);
+    $last_name = ucwords($result['last_name']);
+    $email = strtolower($result['email']);
+    $phone = $result['contact'];
+
+if(isset($_POST['save'])){
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $phone = $_POST['phone'];
+    
+    $updatePersonalDetails = $profile->updatePersonalDetails($firstname, $lastname, $phone, $id);
+    if($updatePersonalDetails == true){
+        $_SESSION['success'] = $updatePersonalDetails['success'];
+    }else{
+        $_SESSION['failed'] = $updatePersonalDetails['failed'];
+    }
+        
+        
+
+}
+
+$accountOverview = $profile->accountOverview($id);
+    $email = strtolower($accountOverview ['email']);
+    $password = $accountOverview ['password'];
+    $status = $accountOverview['status'];
+
+
+?>
