de0fe108c3309a58554f4516ac75f14f06bdcd43
unknown <ianjeru020@gmail.com>
2025-09-02 22:11:36 +0300

    Contains a profile class that manage the database context and interactions of a user profile

---
+++ user/model/profile.php
@@ -0,0 +1,55 @@
+<?php
+include "../db/connection.php";
+class profile extends connection {
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function personalDetails($id){
+        $stmt = "SELECT * FROM customer_profile WHERE id = '$id'";  
+        $query = $this->connection->query($stmt);
+
+        if($query->num_rows == 1){
+            $row = $query->fetch_assoc();
+            $first_name = $row['first_name'];
+            $last_name = $row['last_name'];
+            $email = $row['email'];
+            $contact = $row['contact'];
+
+            return [
+                'first_name'=>$first_name,
+                'last_name'=>$last_name,
+                'email'=>$email,
+                'contact'=>$contact
+            ];
+        }
+    }
+
+    public function updatePersonalDetails(){
+
+    }
+
+    public function accountOverview($id){
+        $stmt = "SELECT * FROM customer_login WHERE id = '$id'";
+        $query = $this->connection->query($stmt);
+
+        if($query->num_rows == 1){
+            $row = $query->fetch_assoc();
+            $email = $row['email'];
+            $password = "**********";
+            $status = $row['status'];
+
+            return[
+                'email'=>$email,
+                'password'=>$password,
+                'status'=>$status
+            ];
+        }
+    }
+
+    public function delete(){
+
+    }
+}
+
+?>
