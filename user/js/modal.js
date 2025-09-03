90e9ad3e751018cdb6504123f33d430c98a68bf2
unknown <ianjeru020@gmail.com>
2025-09-03 23:19:39 +0300

    The modal javascript and styling

---
+++ user/js/modal.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+$(".fa").click(function () {
+            $(".actionModal").show();
+        });
+
+        $("span").click(function () {
+            $(".actionModal").hide();
+        });
+
+        });
