13d7f1bbb035e51c172708acf894aa7f0f1fd3b2
unknown <ianjeru020@gmail.com>
2025-09-15 23:29:29 +0300

    Prevent a deleted user from logging in

--- user/model/auth.php
+++ user/model/auth.php
@@ -7,22 +7,28 @@ include "../db/connection.php";
         }
 
         public function user($username, $password){
-            $stmt = "SELECT * FROM customer_login WHERE (name = '$username' OR email = '$username')
-             AND password = '$password'";
+            $stmt = "SELECT * FROM customer_login WHERE (name = '$username' OR email = '$username') AND password = '$password' AND online_status != 'deleted'";
 
             $query = $this->connection->query($stmt);
             if($query->num_rows == 1){
                 $row = $query->fetch_assoc();
                 $name = $row['name'];
                 $user_id = $row['id'];
-                
-                    return 
-                    [
+                $online_status = $row['online_status'];
+                if($online_status != 'deleted'){
+                    return[
                         'status'=>true,
                         'user_id'=>$user_id,
                         'name'=>$name
                     ];
-            
+                }else{
+                    return 
+                    [
+                        'status'=>false,
+                        'message'=>'No account found.'
+                        
+                    ];
+                }
             }else{
                 return 
                 [
